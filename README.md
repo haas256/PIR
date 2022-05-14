4241c4fcd80b6614556917115fe1da5636120041
haas256 <hildagong@Hildas-MacBook-Pro.local>
2022-05-13 21:10:59 -0400

    Simplify verification

--- README.md
+++ README.md
@@ -1,2 +1,70 @@
-# PIR
-Collusion mitigation in multi-server PIR
+# Coordinator for mitigating collusion in multi-server PIR
+
+## Cost in Berlin EVM
+
+| operation                         | cost in gas |
+| --------------------------------- | ----------- |
+| deployment                        | 4697299     |  
+| deposit                           | 105436      | Average(116836 99736 99736)
+| post requests                     | 405657      | 
+| submit response                   | 97400       | Average(114558 83308 94334)
+| claim service fees                | 33103       |
+| accuse                            | 223766      | Average(240013 225713 205573 224998)
+| verify type 1                     | 61822       | 
+| check triviality                  | 66991 + Chainlink costs| Sum(25439 41552)
+| verify type 2                     | 275279      | Sum(82649 68375 124255)
+
+## Example requests
+### Setup
+>> Addresses of queried servers:
+Server 1: 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2
+Server 2: 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db
+Server 3: 0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB
+
+>> Address of example user: 0x617F2E2fD72FD9D5503197092aC168c91465E7f2
+
+>> Signature scheme: ECDSA (using the curve secp256k1 + SHA3-256)
+Private key: 0x79afbf7147841fca72b45a1978dd7669470ba67abbe5c220062924380c9c364b 
+Public key: (0x3804a19f2437f7bba4fcfbc194379e43e514aa98073db3528ccdbdb642e240, 0x6b22d833b9a502b0e10e58aac485aa357bccd1df6ec0fa4d398908c1ac1920bc)
+
+>> Commitment scheme: SHA-3 hash
+
+### Three queries:
+Query 1: "0x8AA4", with commitment / hash "0x97b76ac95098db47598c9262dc539a7438402f1272d37226c9433dc4cb394d08"
+Query 2: "0x8A24", "0x7e3af5120fde202ac27cab61b9ce3fe66ca03350a6ebd1357206b7470170ec77"
+Query 3: "0x8A26", "0x33e3348b6b3c6765442204236f167c1d019df61bd1c15eed19f7f18f349a087d"
+
+s = ["0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2", "0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db", "0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB"]
+comms = ["0x97b76ac95098db47598c9262dc539a7438402f1272d37226c9433dc4cb394d08", "0x7e3af5120fde202ac27cab61b9ce3fe66ca03350a6ebd1357206b7470170ec77", "0x33e3348b6b3c6765442204236f167c1d019df61bd1c15eed19f7f18f349a087d"]
+
+
+### Three (fake) responses:
+ - server 1
+@0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2
+resp = 35492 0x8AA4
+r-comm = "0x97b76ac95098db47598c9262dc539a7438402f1272d37226c9433dc4cb394d08"
+
+ - server 2
+@0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db
+resp = 35364 0x8A24
+r-comm = "0x7e3af5120fde202ac27cab61b9ce3fe66ca03350a6ebd1357206b7470170ec77"
+
+ - server 3
+@0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB
+resp = 35366 0x8A26
+r-comm = "0x33e3348b6b3c6765442204236f167c1d019df61bd1c15eed19f7f18f349a087d"
+         
+### Accuse
+- Type 1
+Since either query or response works, we can accuse with an exchanged query. 
+eType = 1 (eType is the categories in the implementation; different from the type characterization in the paper)
+evidence = "0x8AA4"
+
+- Type 2
+eType = 3 
+evidence = "0x8AA6" or "0x0000000000000000000000000000000000000000000000000000000000008AA6"
+
+"0x0000000000000000000000000000000000000000000000000000000000008AA4"
+"0x0000000000000000000000000000000000000000000000000000000000008A24"
+"0x0000000000000000000000000000000000000000000000000000000000008A26"
+address(0) "0x0000000000000000000000000000000000000000"
--- contracts/SimpleCoordinator.sol
+++ contracts/SimpleCoordinator.sol
@@ -1,13 +1,14 @@
 // SPDX-License-Identifier: GPL-3.0
 /*
  * @title Collusion-resistant multi-server PIR manager
- * @author Tiantian Gong <tiangong17@gmail.com>
+ * @author Anonym <@gmail.com>
  *
  * @dev Implements a contract managing multi-server PIR services and collusion accusation resolution
  */
 pragma solidity >=0.7.0;
 
 import { BytesLib } from "./BytesLib.sol";
+import "./ECDSA.sol";
 // import "@chainlink/contracts/src/v0.7/ChainlinkClient.sol";
 // contract SimpleCoordinator is ChainlinkClient
 
@@ -36,13 +37,12 @@ contract SimpleCoordinator {
         uint deposit; // deposit amount
         bool active; // if true, deposit satisfys minimum amount
         uint accusationfee; // accusation fee amount
-        uint[] eph_key; // El Gamal key (y, g, p) where y=g^x mod p
         mapping(uint => bool) fined;
         bool isSet; 
     }
     mapping(address => Server) public servers;
 
-    mapping(uint => mapping(address => uint[])) keys;
+    // mapping(uint => mapping(address => uint[])) keys;
     
     // bookkeeping the queries 
     struct Journal {
@@ -50,10 +50,12 @@ contract SimpleCoordinator {
         uint jid; // journal id
         address client; // client address
         uint fee; // service fee amount 
-        uint[3] signKey; // public key for digital signature: (pk, modulus, generator)
         address[] serverList; // list of addresses of queried servers 
+        mapping(address => bytes32) q_comms; // query commitments
+        mapping(address => bytes) q_sigs; // query signatures
         mapping(address => bytes32) r_comms; // response commitments
-        mapping(address => uint[3]) commKey; // (modulus, two public generators)
+        mapping(address => bytes) r_sigs; // response signatures
+        // mapping(address => uint[3]) commKey; // (modulus, two public generators if using Pedersen)
         uint8 rcnt; //response count
         bool[] responded; // indicate response received from each server in serverList
         bool active; 
@@ -70,7 +72,7 @@ contract SimpleCoordinator {
         uint e_type; // evidence type
         // 1 - query; 2 - response; 3 - Type 2 with input being query ; 4 - Type 2 with input being response
         bytes ed; // evidence
-        uint[] m; // message
+        uint[] m; // messages
         uint gasLimit; // the maximum has to be attached for Type 2 function evaluation
         bool isSet;
     }
@@ -85,7 +87,7 @@ contract SimpleCoordinator {
     event Deposited(address indexed payee, uint weiAmount);
     event Withdrawn(address indexed payee, uint weiAmount);
     event Query(address indexed user, uint weiAmount);
-    event Response(uint jid, bytes32 response);
+    event Response(uint jid, bytes32 response, bytes signature);
     event NewAccusation(uint jid, uint pid);
     event EvidenceCollected(uint pid);
     event EvidenceVerification(Proposal p);
@@ -160,6 +162,7 @@ contract SimpleCoordinator {
         }
     }
 
+    /**
     /// @dev update IBE key
     function updateKey(uint[] memory epk) public {
         // require(epk != servers[msg.sender].eph_key);
@@ -183,9 +186,10 @@ contract SimpleCoordinator {
         }
         return eph_keys;
     }
-    
+    */
+
     /// @dev post queries: for convenience, we let query vector and addresses correspond to each other (ordered) but this is not a must, we can have indicator string prepended to each query byte string
-    function postRequests(address[] memory s, uint y, uint p, uint g) public payable {
+    function postRequests(address[] memory s, bytes32[] memory comms, bytes[] memory sigs) public payable {
         require(msg.value > 0); // require positive service fees
         require(s.length > 1); // require querying more than 1 server
         
@@ -194,13 +198,13 @@ contract SimpleCoordinator {
         journals[jcounter].client = msg.sender;
         journals[jcounter].serverList = s;
         journals[jcounter].fee = msg.value;
-        journals[jcounter].signKey = [y,p,g];
         journals[jcounter].active = true;
         journals[jcounter].isSet = true;
         journals[jcounter].rcnt = 0;
         trackBalance += msg.value;
         for (uint8 i=0; i<s.length; i++) {
-            keys[jcounter][s[i]] = servers[s[i]].eph_key;
+            journals[jcounter].q_comms[s[i]] = comms[i];
+            journals[jcounter].q_sigs[s[i]] = sigs[i];
             journals[jcounter].responded.push(false);
         }
         jcounter++; 
@@ -214,7 +218,7 @@ contract SimpleCoordinator {
     /// @dev submit response: 
     /// avoided repeated submissions
     /// does not check answer
-    function submitResponse(bytes32 r_comm, uint p, uint g, uint h, uint jid) public {
+    function submitResponse(bytes32 r_comm, bytes memory r_sig, uint jid) public {
         for (uint8 i=0; i<journals[jid].serverList.length; i++) {
             if (journals[jid].serverList[i] == msg.sender) {
                 require(journals[jid].active == true);
@@ -223,7 +227,7 @@ contract SimpleCoordinator {
                     journals[jid].rcnt++;
                 }
                 journals[jid].r_comms[msg.sender] = r_comm;
-                journals[jid].commKey[msg.sender] = [p,g,h];
+                journals[jid].r_sigs[msg.sender] = r_sig;
                 if (journals[jid].rcnt == journals[jid].serverList.length) {
                     journals[jid].timestamp = block.timestamp; // the timestamp used to schedule payment
                     // schedule transaction fee transfer to the servers: can be troublesome since no guarantee on who is submitting solutions
@@ -233,7 +237,7 @@ contract SimpleCoordinator {
             }
         }
         servers[msg.sender].active = false; 
-        emit Response(jid, r_comm);
+        emit Response(jid, r_comm, r_sig);
     }
 
     /** 
@@ -259,7 +263,67 @@ contract SimpleCoordinator {
         }
     }
 
-    
+    /** 
+     * @dev Byzantine user committing to one message m but sign m'(\neq m)
+     * The query is discarded and service fees automatically distributed to involved servers. 
+     */
+    function badQuery(uint jid, uint8 sid, bytes memory q) public {
+        require(journals[jid].serverList[sid] == msg.sender);
+        bytes32 comm = journals[jid].q_comms[msg.sender];
+        bytes memory sig = journals[jid].q_sigs[msg.sender];
+        if (vrfyHash(comm, q)) {
+            address recovered = ECDSA.recover(comm, abi.encodePacked(sig));
+            if (recovered == journals[jid].client) {
+                // correct signature -> punish false accuser
+                trackBalance -= defaultFines; //servers[sv].deposit;
+                servers[msg.sender].deposit -= defaultFines; 
+                if (servers[msg.sender].deposit < defaultMinDeposit) {
+                    servers[msg.sender].active = false;
+                }
+            } else {
+                // unmatching signature -> distribute the fees and drop the query
+                uint feeShare = journals[jid].fee/journals[jid].serverList.length;
+                require(feeShare > 0);
+                trackBalance -= journals[jid].fee;
+                journals[jid].fee = 0;
+                address payable addr;
+                address addr1;
+                for (uint j=0; j<journals[jid].serverList.length;j++) {
+                    addr1 = journals[jid].serverList[j];
+                    addr = payable(addr1);
+                    addr.transfer(feeShare);
+                }
+                delete journals[jid];
+            }
+        }
+    }
+
+    /** 
+     * @dev Byzantine server committing to one message m but sign m'(\neq m)
+     * The byzantine server is penalized. 
+     */
+    function badResponse(uint jid, uint8 sid, bytes memory resp) public {
+        require(journals[jid].client == msg.sender);
+        require(journals[jid].responded[sid] == true); 
+        
+        address accused = journals[jid].serverList[sid];
+        bytes32 comm = journals[jid].r_comms[accused];
+        bytes memory sig = journals[jid].r_sigs[accused];
+        if (vrfyHash(comm, resp)) {
+            address recovered = ECDSA.recover(comm, abi.encodePacked(sig));
+            if (recovered != accused) {
+                // unmatching signature -> penalize the server
+                trackBalance -= defaultFines; //servers[sv].deposit;
+                servers[accused].deposit -= defaultFines; 
+                if (servers[accused].deposit < defaultMinDeposit) {
+                    servers[accused].active = false;
+                }
+                // delete the corresponding response
+                journals[jid].responded[sid] = false;
+                journals[jid].active = true;
+            }
+        }
+    }
 
     /** 
      * @dev accuse with evidence 
@@ -286,14 +350,12 @@ contract SimpleCoordinator {
     }
 
     /** 
-     * @dev submit auxiliary information for Type 1 evidence (-query and -response)
+     * @dev submit auxiliary information for Type 1 evidence (-query or -response)
      * @param pid proposal id
      * @param etype evidence type
      * @param mg message plaintext
-     * @param a signature part 1 from user; or message in uint 
-     * @param b signature part 2 from user; or randomness used in commitment 
      */
-    function submitNewInfo1(uint pid, uint8 etype, bytes memory mg, uint a, uint b) public {
+    function submitNewInfo1(uint pid, uint8 etype, bytes memory mg) public {
         // make sure the proposal exists and the evidence type matches
         require(proposals[pid].isSet);
         require(proposals[pid].e_type == etype);
@@ -306,16 +368,14 @@ contract SimpleCoordinator {
         for (uint8 i = 0; i < journals[jid].serverList.length; i++) {
             if (journals[jid].serverList[i] == msg.sender) {
                 if (etype == 1) {
-                    if (vrfyElGamalSig(BytesLib.toUint256(mg, 0), journals[jid].signKey[1], journals[jid].signKey[2], journals[jid].signKey[0], a, b)) {
-                        valid = true;
-                    }
+                    comm = journals[jid].q_comms[msg.sender];
                 } else {
                     comm = journals[jid].r_comms[msg.sender];
-                    if (vrfyPedersenComm(uint(comm), journals[jid].commKey[msg.sender][1], journals[jid].commKey[msg.sender][2], a, b, journals[jid].commKey[msg.sender][0])) {
-                        valid = true;
-                    }
                 } 
-                if (BytesLib.equalStorage(proposals[pid].ed,mg) && valid) {
+                if (vrfyHash(comm, mg)) {
+                    valid = true;
+                }
+                if ((BytesLib.equalStorage(proposals[pid].ed,mg) && valid) || (!valid)) {
                     // execute payments: (1) pay the accuser; (2) fine the accused
                     trackBalance -= defaultFines; //servers[sv].deposit;
                     servers[msg.sender].deposit -= defaultFines; 
@@ -344,7 +404,7 @@ contract SimpleCoordinator {
     /** 
      * @dev submit auxiliary information for Type 2 evidence
      */
-    function submitNewInfo2(uint pid, uint8 etype, bytes memory mg, uint a, uint b) public {
+    function submitNewInfo2(uint pid, uint8 etype, bytes memory mg) public {
         // make sure the proposal exists and the evidence type matches
         require(proposals[pid].isSet);
         require(proposals[pid].e_type == etype);
@@ -358,15 +418,13 @@ contract SimpleCoordinator {
         for (uint8 i = 0; i < journals[jid].serverList.length; i++) {
             if (journals[jid].serverList[i] == msg.sender) {
                 if (etype == 3) {
-                    if (!vrfyElGamalSig(m, journals[jid].signKey[1], journals[jid].signKey[2], journals[jid].signKey[0], a, b)) {
-                        return;
-                    }
+                    comm = journals[jid].q_comms[msg.sender];
                 } else {
                     comm = journals[jid].r_comms[msg.sender];
-                    if (!vrfyPedersenComm(uint(comm), journals[jid].commKey[msg.sender][1], journals[jid].commKey[msg.sender][2], a, b, journals[jid].commKey[msg.sender][0])) {
-                        return;
-                    }
                 } 
+                if (!vrfyHash(comm, mg)) {
+                    return;
+                }
                 proposals[pid].m.push(m);
                 // proposals[pid].ed.push(comm);
                 break;
@@ -485,75 +543,25 @@ contract SimpleCoordinator {
 
     // ************************************************************
     // Crypto
-
-    uint constant private U255_MAX_PLUS_1 = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
-
+    
     /** 
-     * Source: https://github.com/witnet/elliptic-curve-solidity/blob/master/contracts/EllipticCurve.sol
-     * @dev Modular exponentiation, b^e % _pp.
-     * Source: https://github.com/witnet/elliptic-curve-solidity/blob/master/contracts/EllipticCurve.sol
-     * @param _base base
-     * @param _exp exponent
-     * @param _pp modulus
-     * @return r such that r = b**e (mod _pp)
+     * @dev Compute hash
      */
-    function expMod(uint _base, uint _exp, uint _pp) internal pure returns (uint) {
-        require(_pp!=0, "Modulus is zero");
-
-        if (_base == 0)
-        return 0;
-        if (_exp == 0)
-        return 1;
-
-        uint r = 1;
-        uint bit = U255_MAX_PLUS_1;
-        assembly {
-        for { } gt(bit, 0) { }{
-            r := mulmod(mulmod(r, r, _pp), exp(_base, iszero(iszero(and(_exp, bit)))), _pp)
-            r := mulmod(mulmod(r, r, _pp), exp(_base, iszero(iszero(and(_exp, div(bit, 2))))), _pp)
-            r := mulmod(mulmod(r, r, _pp), exp(_base, iszero(iszero(and(_exp, div(bit, 4))))), _pp)
-            r := mulmod(mulmod(r, r, _pp), exp(_base, iszero(iszero(and(_exp, div(bit, 8))))), _pp)
-            bit := div(bit, 16)
-        }
-        }
-
-        return r;
+    function computeHash(bytes memory _str) internal pure returns (bytes32) {
+        return keccak256(abi.encodePacked(_str));
     }
-
+    
     /** 
-     * @dev Verify Pedersen commitment
-     * @param g generator, public
-     * @param h generator, public
+     * @dev Verify hash
      */
-    function vrfyPedersenComm(uint comm, uint g, uint h, uint m, uint r, uint modulus) internal pure returns (bool) {
+    function vrfyHash(bytes32 comm, bytes memory _str) internal pure returns (bool) {
+        
         bool valid = false;
-        uint c1 = expMod(g,m,modulus);
-        uint c2 = expMod(h,r,modulus);
-        if (comm == mulmod(c1,c2,modulus)) {
+        bytes32 c = keccak256(abi.encodePacked(_str));
+        if (comm == c) {
             valid = true;
         }
         return valid;
-    }
-
-    /** 
-     * @dev El Gamal signature verification
-     * @param _m message
-     * @param _a signature part 1 = g^r mod p
-     * @param _b signature part 2 = (H(m) - _xa) r^-1 mod (p-1)
-     * _x private key
-     * @param _y public key
-     * @param g generator
-     */
-    function vrfyElGamalSig(uint _m, uint p, uint g, uint _y, uint _a, uint _b) internal pure returns (bool) {
-        if (_a < p && _b < p-1) {
-            uint _ya = expMod(_y, _a, p);
-            uint _ab = expMod(_a, _b, p);
-            uint m = mulmod(_ya, _ab, p);
-            uint _gm = expMod(g, _m, p);
-            return (m == _gm);
-        } else {
-            return false;
-        }
     }
 
 }
